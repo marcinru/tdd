89699d4a8f60606414fc274500d9d6e0123d7e46
Marcin Er <mar.rudzki@gmail.com>
2023-07-06 11:49:49 +0200

    Add initial setup and 1st test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# tdd
+# TDD
 Min TDD setup
+
+1. Close this repo
+2. `npm install` or `yarn`
+3. `npm test` or `npm t`
---
+++ isLeapYear.test.js
@@ -0,0 +1,5 @@
+import { isLeapYear } from './isLeapYear'
+
+test('2000 is leap year', () => {
+    expect(isLeapYear(2000)).toBe(true)
+})
