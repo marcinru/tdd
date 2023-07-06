df083ed64fa7b1ed0ec43b50c12495ab9718ab0b
Marcin Er <mar.rudzki@gmail.com>
2023-07-06 12:18:20 +0200

    Add tests for isLeapYear

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # TDD
 Min TDD setup
 
+## Pre-requirements
+1. [Node.js](https://nodejs.org/en) installed
+
+## Setup
 1. Close this repo
 2. `npm install` or `yarn`
 3. `npm test` or `npm t`
--- isLeapYear.test.js
+++ isLeapYear.test.js
@@ -1,5 +1,25 @@
 import { isLeapYear } from './isLeapYear'
 
+test('1900 is not leap year', () => {
+    expect(isLeapYear(1900)).toBe(false)
+})
+
 test('2000 is leap year', () => {
+    expect(isLeapYear(2000)).toBe(true)
+})
+
+test('2001 is not leap year', () => {
+    expect(isLeapYear(2001)).toBe(false)
+})
+
+test('2004 is leap year', () => {
+    expect(isLeapYear(2000)).toBe(true)
+})
+
+test('2049 is not leap year', () => {
+    expect(isLeapYear(2001)).toBe(false)
+})
+
+test('2080 is leap year', () => {
     expect(isLeapYear(2000)).toBe(true)
 })
