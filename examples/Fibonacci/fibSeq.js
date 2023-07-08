20e02c4ff4d12882698158359a16b0c99d0df2c0
Marcin Er <mar.rudzki@gmail.com>
2023-07-08 15:28:23 +0200

    Finish the Fibonacci sequence

--- examples/Fibonacci/fibSeq.js
+++ examples/Fibonacci/fibSeq.js
@@ -1,3 +1,7 @@
 export const fibSeq = (n) => {
-  return [0, 1]
+  const fib = [0, 1]
+  for (let i = 2; i < n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2])
+  }
+  return fib
 }
--- examples/Fibonacci/fibSeq.test.js
+++ examples/Fibonacci/fibSeq.test.js
@@ -1,11 +1,15 @@
 import {fibSeq} from './fibSeq';
 
-describe('fibSeq', function () {
+describe('fibSeq(n)', function () {
     it('returns [0, 1] for n = 2', () => {
         expect(fibSeq(2)).toEqual([0, 1])
     })
 
     it('returns [0, 1, 1] for n = 3', () => {
         expect(fibSeq(3)).toEqual([0, 1, 1])
+    })
+
+    it('returns [0, 1, 1, 2, 3, 5, 8, 13] for n = 8', () => {
+        expect(fibSeq(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13])
     })
 });
