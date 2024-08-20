86aa8a99c343aef1d1cdde8b241f1e829f4835eb
Marcin Er <mar.rudzki@gmail.com>
2024-08-20 13:16:04 +0200

    add intervals

--- experimental/play.js
+++ experimental/play.js
@@ -1,7 +1,21 @@
 import { styleText } from "node:util";
+import { stdout } from "node:process";
+import { log } from "node:console";
 
-const lyrics = `I see ${styleText('green', 'trees of green')}
-${styleText('red', 'Red roses')} too
-I see them ${styleText('bgBlue', ' bloom ')}🌸`;
+const lyrics = [
+  "I see 🌳 ",
+  styleText("green", "trees of green\n"),
+  `${styleText("red", "Red roses")}🌹 too\n`,
+  `I see them ${styleText("bgBlue", " bloom ")}🌸\n`,
+  "For me and you",
+];
 
-console.log(lyrics);
+log("🎹");
+let i = 0;
+const interval = setInterval(() => {
+  if (i === lyrics.length - 1) {
+    clearInterval(interval);
+  }
+  stdout.write(lyrics[i]);
+  i++;
+}, 2000);
